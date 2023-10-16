039ebc471d2cbaa2e28ed2717d68651e3c6ca902
slatohamid <hslato@gmail.com>
2023-10-16 12:06:19 +0200

    add vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
-const path = require("path");
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-          '@': require('path').resolve(__dirname, 'src')
-        }
-      },
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-})```
+  base: "tim-berners-lee",
+});
